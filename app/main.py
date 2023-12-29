175e810019381443c839f1e4941c575d1338cf5b
Sepehr Safa <safasepehr@gmail.com>
2023-12-28 22:54:51 -0500

    fixed cros and doc ids

--- app/main.py
+++ app/main.py
@@ -2,21 +2,50 @@
 
 from app.config import settings
 from app.models.user import UserAccount
-from app.routes import (auth_router, business_router, user_group_router,
-                        user_router, wallet_router, ipg_router, wallet_transactions_router)
+from app.routes import (
+    auth_router,
+    business_router,
+    user_group_router,
+    user_router,
+    wallet_router,
+    ipg_router,
+    wallet_transactions_router,
+)
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from tortoise.contrib.fastapi import register_tortoise
+from fastapi.middleware.cors import CORSMiddleware
 
-from owjcommon.exceptions import (OWJException, http_exception_handler,
-                                  owj_exception_handler,
-                                  request_validation_exception_handler)
+from owjcommon.exceptions import (
+    OWJException,
+    http_exception_handler,
+    owj_exception_handler,
+    request_validation_exception_handler,
+)
+
+
+def custom_generate_unique_id(route):
+    return f"{route.tags[0]}-{route.name}"
+
 
 app = FastAPI(
     title="OWJ CRS Account API",
     description="This is the API documentation for the OWJ CRS Account Service.",
     version="1.0.0",
+    generate_unique_id_function=custom_generate_unique_id,
+    servers=[
+        {"url": "http://localhost:8000", "description": "Local environment"},
+        {"url": "https://account.owj.app", "description": "Production environment"},
+    ],
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 BASE_PREFIX = "/api/account/v1"
@@ -25,7 +54,9 @@ app.include_router(user_router, prefix=BASE_PREFIX + "/user")
 app.include_router(user_group_router, prefix=BASE_PREFIX + "/group")
 app.include_router(business_router, prefix=BASE_PREFIX + "/business")
 app.include_router(auth_router, prefix=BASE_PREFIX + "/auth")
-app.include_router(wallet_transactions_router, prefix=BASE_PREFIX + "/wallet/transaction")
+app.include_router(
+    wallet_transactions_router, prefix=BASE_PREFIX + "/wallet/transaction"
+)
 app.include_router(wallet_router, prefix=BASE_PREFIX + "/wallet")
 app.include_router(ipg_router, prefix=BASE_PREFIX + "/ipg")
 
